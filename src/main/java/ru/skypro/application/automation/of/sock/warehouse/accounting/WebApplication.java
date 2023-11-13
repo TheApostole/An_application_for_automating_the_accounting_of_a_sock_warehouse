c7e68783976411c26298796a8b96368eb4a9beaa
TheApostole <yaloza99@mail.ru>
2023-11-13 18:45:21 +0300

    Приложение для автоматизации учёта носков на складе магазина

---
+++ src/main/java/ru/skypro/application/automation/of/sock/warehouse/accounting/WebApplication.java
@@ -0,0 +1,10 @@
+package ru.skypro.application.automation.of.sock.warehouse.accounting;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
